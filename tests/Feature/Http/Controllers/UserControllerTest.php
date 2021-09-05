cfbd5148ade17dc04b6c38c99bffbe518e6e8188
Zainal Hasan <zhanang19@gmail.com>
2021-09-05 08:32:29 +0700

    feat: add test for user management

---
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use JMac\Testing\Traits\AdditionalAssertions;
+use Tests\TestCase;
+
+/**
+ * @see \App\Http\Controllers\UserController
+ */
+class UserControllerTest extends TestCase
+{
+    use AdditionalAssertions;
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_render_lists_user_page(): void
+    {
+        $response = $this->actingAs($this->admin())
+            ->get(route('user.index'));
+
+        $response->assertOk();
+        $response->assertSeeText(trans('users.plural-name'));
+        $response->assertViewIs('user.index');
+    }
+
+    /** @test */
+    public function it_can_process_datatable_user_ajax(): void
+    {
+        $this->assertHasDatatable(route('user.index'));
+    }
+
+    /** @test */
+    public function it_can_process_to_delete_user(): void
+    {
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($this->admin())
+            ->delete(route('user.destroy', $user));
+
+        $response->assertRedirect(route('user.index'));
+
+        $this->assertSoftDeleted($user);
+    }
+}
