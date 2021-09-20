35f3765ba5d254a588fe8d39b116f63fd99fa464
Zainal Hasan <zhanang19@gmail.com>
2021-09-20 23:55:22 +0700

    wip

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -42,6 +42,8 @@
 
             <ul class="c-header-nav ml-auto">
 
+                @livewire('user-notification')
+
                 @include('layouts.partials.change-language')
 
                 <li class="c-header-nav-item dropdown">
